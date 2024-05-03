7154419df911afb65c617a96778e792f55a9de62
NoMurcy <m4tr04@gmail.com>
2024-05-03 20:47:03 +0200

    Dodanie funkcji zamieniającej współrzędne XYZ na NEU
    Ta transformacja jeszcze nie do końca działa
    a) w funkcji odczytu pliku należy naprawić przypisywanie do X0 Y0 Z0 pierwszych 3 wartości z pierwszego wiersza wczytywanego pliku
    b) w funkcji xyz2neup należy poprawić obliczenia tak, aby działały na listach i były przeprowadzane dla każdej współrzędnej

--- projekt1.py
+++ projekt1.py
@@ -5,7 +5,7 @@
 # - BL(GRS80, WGS84, ew. Krasowski) -> 1992
 
 from math import sin, cos, tan, sqrt, atan, atan2, radians
-from numpy import array, savetxt, column_stack
+from numpy import array, savetxt, column_stack, dot
 from argparse import ArgumentParser
 
 
@@ -39,8 +39,9 @@ class Transformacje:
         with open(sciezka, 'r') as file:
             lines = file.readlines()
 
-            if trans == 'XYZ2BLH' or trans == 'XYZ2NEUP' or trans == 'BLH2XYZ':
+            if trans == 'XYZ2BLH' or trans == 'BLH2XYZ':
                 lists = {"1": [], "2": [], "3": []}
+                # lists = []
                 for line in lines:
                     parts = line.split()
                     lists["1"].append(float(parts[0]))
@@ -53,7 +54,41 @@ class Transformacje:
                     parts = line.split()
                     lists["1"].append(float(parts[0]))
                     lists["2"].append(float(parts[1]))
+
+            elif trans == 'XYZ2NEUP':
+                lists = {"1": [], "2": [], "3": [],
+                         "X0": [], "Y0": [], "Z0": []}
+                parts = lines[0].split()
+                lists["X0"].append(float(parts[0]))
+                lists["Y0"].append(float(parts[1]))
+                lists["Z0"].append(float(parts[2]))
+
+                for line in lines[1:]:
+                    parts = line.split()
+                    lists["1"].append(float(parts[0]))
+                    lists["2"].append(float(parts[1]))
+                    lists["3"].append(float(parts[2]))
+
             return lists
+
+    def transpose(self, matrix):
+        return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+
+    def matrix_multiply(self, matrix1, matrix2):
+        # if len(matrix1[0]) != len(matrix2):
+        #     raise ValueError("Niepoprawne wymiary macierzy do mnożenia")
+
+        result = []
+        for i in range(len(matrix1)):
+            row = []
+            for j in range(len(matrix2[0])):
+                sum_value = 0
+                for k in range(len(matrix2)):
+                    sum_value += matrix1[i][k] * matrix2[k][j]
+                row.append(sum_value)
+            result.append(row)
+
+        return result
 
     def hirvonen(self, X, Y, Z):
         fi_list = []
@@ -174,60 +209,97 @@ class Transformacje:
 
         return results_x2000, results_y2000
 
-    def Rneu(fa, la):
-        R = array([[-sin(fa)*cos(la), -sin(la), cos(fa)*cos(la)],
-                   [-sin(fa)*sin(la), cos(la), cos(fa)*sin(la)],
-                   [cos(fa), 0, sin(fa)]])
+    def Rneu(self, phi, lam):
+        R = array([[-sin(phi)*cos(lam), -sin(lam), cos(phi)*cos(lam)],
+                   [-sin(phi)*sin(lam), cos(lam), cos(phi)*sin(lam)],
+                   [cos(phi), 0, sin(phi)]])
         return(R)
 
     def xyz2neup(self, X, Y, Z, X0, Y0, Z0):
-        neu = []
-        p = sqrt(X0**2 + Y0**2)
-        fi = atan(Z0 / (p*(1 - self.e2)))
-        while True:
-            N = self.a/sqrt(1-self.e2 * sin(fi)**2)
-            h = (p / cos(fi)) - N
-            fi_poprzednia = fi
-            fi = atan((Z0 / p)/(1-((N * self.e2)/(N + h))))
-            if abs(fi_poprzednia - fi) < (0.000001/206265):
-                break
-        N = self.a/sqrt(1-self.e2 * sin(fi)**2)
-        h = p/cos(fi) - N
-        lam = atan(Y0 / X0)
+        neu_results = []
 
-        R_neu = self.Rneu(fi, lam)
-        X_sr = [X - X0, Y - Y0, Z - Z0]
-        X_neu = R_neu.T@X_sr
-        neu.append(X_neu)
+        fi_list = []
+        l_list = []
+        h_list = []
+        for x, y, z, x0, y0, z0 in zip(X, Y, Z, X0, Y0, Z0):
+            l = atan2(y, x)
+            p = sqrt(x**2 + y**2)
+            f = atan(z / (p * (1 - self.e2)))
 
-        return(neu)
+            while True:
+                N = self.a / sqrt(1 - self.e2 * sin(f)**2)
+                h = p / cos(f) - N
+                fs = f
+                f = atan(z / (p * (1 - (self.e2 * (N / (N + h))))))
+                if abs(fs - f) < (0.000001/206265):
+                    break
+
+            fi_list.append(f)
+            l_list.append(l)
+            h_list.append(h)
+            print(fi_list)
+            print(l_list)
+            print(h_list)
+        for element in fi_list, h_list:
+            R = array([[-sin(f) * cos(l), sin(l), cos(f) * cos(l)],
+                       [-sin(f) * sin(l),  cos(l), cos(f) * sin(l)],
+                       [cos(f), 0, sin(f)]])
+            xyz = array([x, y, z]).T
+            xyz0 = array([x0, z0, y0]).T
+            xyzt = xyz - xyz0
+            neu = R.T @ xyzt
+
+        neu_results.append(neu)
+
+        return neu_results
 
     def licz(self, plik_input, trans):
         dane = self.plik(plik_input, trans)  # Wczytanie danych z pliku
-        X = dane["1"]
-        Y = dane["2"]
-        # Z = dane["3"]
+        # X = dane[:,0]
+        # Y = dane[:,1]
+        # Z = dane[:,2]
 
         if trans == 'XYZ2BLH':
+            X = dane["1"]
+            Y = dane["2"]
             Z = dane["3"]
             wyniki = self.hirvonen(X, Y, Z)
             savetxt(f"results{trans}_{args.el}.txt",
                     column_stack(wyniki), delimiter=' ')
             return wyniki
+
         if trans == 'BLH2XYZ':
+            X = dane["1"]
+            Y = dane["2"]
             Z = dane["3"]
             wyniki = self.flh2xyz(X, Y, Z)
             savetxt(f"results{trans}_{args.el}.txt",
                     column_stack(wyniki), delimiter=' ')
             return wyniki
-        # if trans == 'XYZ2NEUp':
-        #     wyniki = self.hirvonen(X, Y, Z)
+
+        if trans == 'XYZ2NEUP':
+            X = dane["1"]
+            Y = dane["2"]
+            Z = dane["3"]
+            X0 = dane["X0"]
+            Y0 = dane["Y0"]
+            Z0 = dane["Z0"]
+            wyniki = self.xyz2neup(X, Y, Z, X0, Y0, Z0)
+            savetxt(f"results{trans}_{args.el}.txt",
+                    column_stack(wyniki), delimiter=' ')
+            return wyniki
+
         if trans == 'BL2PL2000':
+            X = dane[:, 0]
+            Y = dane[:, 1]
             wyniki = self.pl2000(X, Y)
             savetxt(f"results{trans}_{args.el}.txt",
                     column_stack(wyniki), delimiter=' ')
             return wyniki
+
         if trans == 'BL2PL1992':
+            X = dane[:, 0]
+            Y = dane[:, 1]
             wyniki = self.pl1992(X, Y)
             savetxt(f"results{trans}_{args.el}.txt",
                     column_stack(wyniki), delimiter=' ')
@@ -241,21 +313,13 @@ if __name__ == '__main__':
     parser.add_argument('-t', type=str)
     args = parser.parse_args()
 
-    i = 0
-    try:
-        while i == 0:
-
-            if args.el is None:
-                args.el = input(
-                    'Na jakiej elpisoidzie wykonywane będą obliczenia?: ').upper()
-            if args.wsp is None:
-                args.wsp = input('Wklej ścieżkę do pliku txt z danymi: ')
-            if args.t is None:
-                args.t = input('Jaką transformację chcesz wykonać?: ').upper()
-            obiekt = Transformacje(args.el.upper())
-            dane = obiekt.licz(args.wsp, args.t.upper())
-            print(dane)
-            i = i + 1
-
-    finally:
-        print('FIN :)')
+    if args.el is None:
+        args.el = input(
+            'Na jakiej elpisoidzie wykonywane będą obliczenia?: ').upper()
+    if args.wsp is None:
+        args.wsp = input('Wklej ścieżkę do pliku txt z danymi: ')
+    if args.t is None:
+        args.t = input('Jaką transformację chcesz wykonać?: ').upper()
+    obiekt = Transformacje(args.el.upper())
+    dane = obiekt.licz(args.wsp, args.t.upper())
+    print(dane)
